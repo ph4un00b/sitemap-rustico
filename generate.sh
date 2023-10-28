d782dd3352a4bf6d52473fe89e217f97bb10f5ea
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-10-28 11:55:34 -0600

    refactor: ci

---
+++ generate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+chmod +x scripts/gen-articles.sh
+./scripts/gen-articles.sh > generated/gen-articles.txt
+
+chmod +x scripts/gen-weekly.sh
+./scripts/gen-weekly.sh > generated/gen-weekly.txt
+
+chmod +x scripts/gen-home.sh
+./scripts/gen-home.sh > generated/gen-home.txt
+
+chmod +x scripts/gen-book.sh
+./scripts/gen-book.sh > generated/gen-book.txt
+
+cargo run --bin tags > generated/gen-tags.txt
+
+cat generated/gen-articles.txt generated/gen-weekly.txt generated/gen-book.txt generated/gen-home.txt generated/gen-tags.txt > generated/dates_and_paths.txt
+
+# cat generated/dates_and_paths.txt
+cargo run --bin generate
