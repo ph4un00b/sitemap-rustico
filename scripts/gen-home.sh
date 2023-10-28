e5a738c849a38f54a1bd8cdb9128864a91885ec6
ph4u <1057021+ph4un00b@users.noreply.github.com>
2023-10-28 17:15:25 -0600

    fix: namespace

--- scripts/gen-home.sh
+++ scripts/gen-home.sh
@@ -7,7 +7,7 @@ initialDir=$(pwd)
 cd home
 
 # Run command
-git ls-tree -r --name-only HEAD --full-tree src/pages | xargs -I {} git log -1 --format="%aI,book/{}" {} | grep "\.rs$" | awk -F "." '{print $1}'
+git ls-tree -r --name-only HEAD --full-tree src/pages | xargs -I {} git log -1 --format="%aI,home/{}" {} | grep "\.rs$" | awk -F "." '{print $1}'
 
 # Change back to initial directory
 cd "$initialDir"
